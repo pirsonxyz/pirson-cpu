dc569272ed12cc694b562c9492f1c4f9cd6a09de
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-08-09 18:46:35 -0400

    add some source code to read the example file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let source = "example";
+    let file = fs::read_to_string(source).expect("File not found");
+    let parts: Vec<&str> = file.split('_').collect();
+    println!("{:#?}", parts);
 }
